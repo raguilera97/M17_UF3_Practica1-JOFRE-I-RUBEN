7fceceb466bcb3a2083198873bb7abdc1897cb08
raguilera <rubentus1@gmail.com>
2022-04-28 13:13:10 +0200

    UI resum recursos

--- RTS Reshu/Assets/Scripts/Almacen.cs
+++ RTS Reshu/Assets/Scripts/Almacen.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Almacen : MonoBehaviour
 {
     public int rock;
     public int food;
+
+    public Text foodT, rockT;
+    private void Update()
+    {
+        foodT.text = food.ToString();
+        rockT.text = rock.ToString();
+
+    }
 
     internal void FillAlmacen(int[] backpack)
     {
