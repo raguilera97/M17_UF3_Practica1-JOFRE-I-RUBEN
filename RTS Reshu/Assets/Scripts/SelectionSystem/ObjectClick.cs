5e5c8b7dc6383fd13bda6123af712760786b982f
raguilera <rubentus1@gmail.com>
2022-05-05 02:50:45 +0200

    Marc: He solucionado casi todos tus bugs de la ia, faltan algunos, -Desactivar el trigger para que no haga doble hit al volver -Algun que otro bug visual

--- RTS Reshu/Assets/Scripts/SelectionSystem/ObjectClick.cs
+++ RTS Reshu/Assets/Scripts/SelectionSystem/ObjectClick.cs
@@ -50,7 +50,6 @@ public class ObjectClick : MonoBehaviour
                 {
                     if (!hitObject.name.Contains("Enemy"))
                     {
-<<<<<<< HEAD
                         if (Input.GetKey(KeyCode.LeftShift))
                         {
                             Selections.Instance.ShiftClickSelect(hitObject.GetComponent<Unit>());
@@ -59,14 +58,6 @@ public class ObjectClick : MonoBehaviour
                         {
                             Selections.Instance.SelectClick(hitObject.GetComponent<Unit>());
                         }
-=======
-                        Selections.Instance.ShiftClickSelect(hitObject.GetComponent<Unit>());
-                    }
-                    else
-                    {
-                        Selections.Instance.SelectClick(hitObject.GetComponent<Unit>());
-                        
->>>>>>> CombatSys
                     }
                    
                 }
